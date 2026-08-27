crdroidandroid/android_device_xiaomi_diting

diting: Include MiuiCamera if existing

BoardConfig.mk
@@ -12,6 +12,9 @@ include vendor/xiaomi/diting/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/diting
 
+# MiuiCamera
+-include device/xiaomi/miuicamera-diting/BoardConfig.mk
+
 # Audio
 AUDIO_FEATURE_ENABLED_CIRRUS_CALIBRATION_RESISTANCE := true
 

device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/diting/diting-vendor.mk)
 
+# MiuiCamera
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-diting/device.mk)
+
 # Blur
 TARGET_ENABLE_BLUR := true
 

Xiaomi-12T-Pro-Developments/device_xiaomi_diting

BoardConfig.mk
@@ -15,6 +15,9 @@ include vendor/xiaomi/diting/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/diting
 
+# MiuiCamera
+-include device/xiaomi/miuicamera-cupid/BoardConfig.mk
+
 # Audio
 AUDIO_FEATURE_ENABLED_CIRRUS_CALIBRATION_RESISTANCE := true
 

device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/diting/diting-vendor.mk)
 
+# MiuiCamera
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-cupid/device.mk)
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_waipio_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/mixer_paths_waipio_mtp.xml \